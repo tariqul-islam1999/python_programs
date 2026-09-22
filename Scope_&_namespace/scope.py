5128dcb61a70dc7a7f4550fcd4f21d2d2bf75735
tariqul-islam1999 <tariqulfaysal@gmail.com>
2026-09-23 02:07:51 +0600

    updating variable scope practice

---
+++ Scope_&_namespace/scope.py
@@ -0,0 +1,119 @@
+"""
+
+Variable Scopes in Function
+----------------------------
+understanding the scopes in python & their uses.
+
+There ase 4 types of scopes in python. Such as -
+1. Local
+2. Global (keyword)
+3. Non-Local (keyword) 
+4. Build-in
+
+"""
+
+# Local Scope
+
+def example():
+    val = 10 # val is a local scope variable 
+    print(val)
+example()
+
+# Global Scope1
+balance=1000
+def bank():
+    deposit=500
+    print(f"New balance - {deposit+balance}") # here balance's value came from global scope
+bank()
+
+#------------------------------------------------------
+
+def bank():
+    print(f"balance - {balance}") # here function can access the value of global variable
+bank()
+
+#-------------------------------------------------------
+
+def bank():
+    global balance # for manipulate the global value, we should use (global) keyword. Otherwise we can't
+    withdrawal = 300
+    balance -= withdrawal
+    print(balance)
+bank()
+
+#-------------------------------------------------------
+
+# nested function operations with variable scope
+x=10
+def outter():
+    x=12
+    def inner():
+        x=8
+        print(f"from inner {x}") # Here inner x=8 will print 1st , then outter x=12 & last global x=10
+        return                  
+    inner()
+    print(f"from ouuter {x}")
+    return
+outter()
+print(f"from global {x}")
+
+#--------------------------------------------------------
+
+x=10
+def outter():
+    x=12
+    def inner():
+        print(f"from inner {x}") # Here inner x=12 will print 1st , then outter x=12 & last global x=10
+        return                  
+    inner()
+    print(f"from ouuter {x}")
+    return
+outter()
+print(f"from global {x}")
+
+#--------------------------------------------------------
+
+x=10
+def outter():
+    def inner():
+        print(f"from inner {x}") # Here inner x=10 will print 1st , then outter x=10 & last global x=10
+        return                  
+    inner()
+    print(f"from ouuter {x}")
+    return
+outter()
+print(f"from global {x}")
+
+#----------------------------------------------------------
+
+x=10
+def outter():
+    x=12
+    def inner():
+        global x
+        x=8
+        print(f"from inner {x}") # Here inner x=8 will print 1st , then outter x=12 & last global x=8
+        return                  
+    inner()
+    print(f"from ouuter {x}")
+    return
+outter()
+print(f"from global {x}")
+
+#-----------------------------------------------------------
+
+# Non-Local Scope
+
+x=10
+def outter():
+    x=12
+    def inner():
+        nonlocal x # nonlocal keyword means that one scope out & change the outter function
+        x=8
+        print(f"from inner {x}") # Here inner x=8 will print 1st , then outter x=8 & last global x=10
+        return                  
+    inner()
+    print(f"from ouuter {x}")
+    return
+outter()
+print(f"from global {x}")
